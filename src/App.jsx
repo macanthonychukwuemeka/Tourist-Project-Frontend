52deb61fbaf5df318e28e28f0cf8e21fdf57e1b5
MAC-ANTHONY <macanthonygreat@gmail.com>
2024-03-24 21:16:13 +0000

    Turist directory frontend first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+
+import { Appbar } from "";
+function App() {
+  return <></>;
+}
+
+export default App;
+// echo "# Tourist-Project-Frontend" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/macanthonychukwuemeka/Tourist-Project-Frontend.git
+// git push -u origin main
