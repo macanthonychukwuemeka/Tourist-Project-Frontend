f90a49c30c28b2aaaf745ae0c5f14ec178f28528
MAC-ANTHONY <macanthonygreat@gmail.com>
2024-04-01 23:45:34 +0100

    new changes

--- src/diary/Diary.jsx
+++ src/diary/Diary.jsx
@@ -1,7 +1,20 @@
+import { Box } from "@mui/material";
 import React from "react";
+import DiaryItem from "./DiaryItem";
 
 const Diary = () => {
-  return <div>Diary</div>;
+  return (
+    <Box
+      display="flex"
+      flexDirection={"column"}
+      padding={3}
+      justifyContent="center"
+      alignItems={"center"}
+    >
+      {[12345]}
+      <DiaryItem />
+    </Box>
+  );
 };
 
 export default Diary;
--- src/diary/DiaryItem.jsx
+++ src/diary/DiaryItem.jsx
@@ -1,7 +1,56 @@
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import React from "react";
-
+// import { EditLocationAltIcon } from "@mui/icons-material";
+<EditLocationAltIcon />;
 const DiaryItem = () => {
-  return <div>DiaryItem</div>;
+  return (
+    <Card
+      sx={{
+        Width: "50%",
+        height: "50vh",
+        margin: 1,
+        Padding: 1,
+        display: "flex",
+        flexDirection: "column",
+        boxShadow: "5px 5px 10px #ccc",
+      }}
+    >
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            {/* {<EditLocationAltIcon />} */}
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        height="194"
+        src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDgxODR8MHwxfHNlYXJjaHwxMHx8bW91bnRhaW58ZW58MHx8fHwxNzExOTIzMDQ1fDA&ixlib=rb-4.0.3&q=80&w=1080"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default DiaryItem;
