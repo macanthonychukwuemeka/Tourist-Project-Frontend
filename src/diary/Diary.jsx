2938a05b178cd5e78fe6db1e54e857dde1ea06d1
MAC-ANTHONY <macanthonygreat@gmail.com>
2024-04-02 16:06:07 +0100

    got all prodcut from server

--- src/diary/Diary.jsx
+++ src/diary/Diary.jsx
@@ -1,8 +1,16 @@
 import { Box } from "@mui/material";
 import React from "react";
 import DiaryItem from "./DiaryItem";
+import { useEffect } from "react";
+import { getAllPosts } from "../api-helpers/helpers";
 
 const Diary = () => {
+  useEffect(() => {
+    getAllPosts()
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Box
       display="flex"
