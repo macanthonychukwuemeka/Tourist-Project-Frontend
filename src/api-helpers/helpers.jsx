a72e038b5200520a6c3a9c3ae39657a8154368d9
MAC-ANTHONY <macanthonygreat@gmail.com>
2024-04-11 23:57:14 +0100

    new commit

--- src/api-helpers/helpers.jsx
+++ src/api-helpers/helpers.jsx
@@ -94,16 +94,18 @@
 
 import axios from "axios";
 export const getAllPosts = async () => {
+  // axios.defaults.baseURL = "http://localhost:5000";
+  // the port value is saved under the axios as default at the main directory
   const res = await axios.get("/posts");
   if (res.status !== 200) {
     return console.log("Some Error Occurred");
   }
-
   const data = res.data;
   return data;
 };
 
 export const sendAuthRequest = async (signup, data) => {
+  // This line declares an asynchronous function named sendAuthRequest which takes two parameters: signup, a boolean indicating whether the user is signing up (true) or logging in (false), and data, an object containing user information such as name, email, and password.
   const res = await axios.post(`/user/${signup ? "signup" : "login"}/`, {
     name: data.name ? data.name : "",
     email: data.email,
